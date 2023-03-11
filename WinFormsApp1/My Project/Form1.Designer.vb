26d9af09e6a081aa099db434eae5d3f6dbafaa0e
Shakir <shakirali.sacube@gmail.com>
2023-03-11 20:05:22 +0530

    Connected with an Sqlite3 database
    	- Also added gitignore

---
+++ WinFormsApp1/My Project/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.UsernameTB = New System.Windows.Forms.TextBox()
+        Me.PasswordTB = New System.Windows.Forms.TextBox()
+        Me.LoginBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(245, 130)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(247, 204)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Password"
+        '
+        'UsernameTB
+        '
+        Me.UsernameTB.Location = New System.Drawing.Point(360, 130)
+        Me.UsernameTB.Name = "UsernameTB"
+        Me.UsernameTB.Size = New System.Drawing.Size(196, 27)
+        Me.UsernameTB.TabIndex = 1
+        '
+        'PasswordTB
+        '
+        Me.PasswordTB.Location = New System.Drawing.Point(360, 207)
+        Me.PasswordTB.Name = "PasswordTB"
+        Me.PasswordTB.Size = New System.Drawing.Size(196, 27)
+        Me.PasswordTB.TabIndex = 1
+        '
+        'LoginBtn
+        '
+        Me.LoginBtn.Location = New System.Drawing.Point(319, 292)
+        Me.LoginBtn.Name = "LoginBtn"
+        Me.LoginBtn.Size = New System.Drawing.Size(94, 29)
+        Me.LoginBtn.TabIndex = 2
+        Me.LoginBtn.Text = "Login"
+        Me.LoginBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LoginBtn)
+        Me.Controls.Add(Me.PasswordTB)
+        Me.Controls.Add(Me.UsernameTB)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "VendIT (Vending Machine Management System)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents UsernameTB As TextBox
+    Friend WithEvents PasswordTB As TextBox
+    Friend WithEvents LoginBtn As Button
+End Class
---
+++ WinFormsApp1/My Project/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SQLite
+Imports System.IO
+
+Public Class Form1
+    Dim path As String = Directory.GetCurrentDirectory()
+    Dim dbPath As String = IO.Path.Combine(path, "..\..\..\database.db")
+    Dim connString As String = $"Data Source={dbPath}"
+    Dim conn As New SQLiteConnection(connString)
+
+    Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
+        Try
+            conn.Open()
+            Dim cmd As New SQLiteCommand("SELECT firstName FROM Users;", conn)
+            Dim reader As SQLiteDataReader = cmd.ExecuteReader()
+
+            While reader.Read()
+                MessageBox.Show(reader("firstName"), "User Names")
+            End While
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error")
+        Finally
+            conn.Close()
+        End Try
+
+    End Sub
+End Class
